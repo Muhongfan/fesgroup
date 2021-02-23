47790aa500549c269487d68be92260f8c058ba8e
Muhongfan <amber1216@live.cn>
2021-02-22 23:21:45 -0500

    Play video pages add intro&menu

--- 诚信理财/pages/sub/play/play.js
+++ 诚信理财/pages/sub/play/play.js
@@ -11,8 +11,12 @@ Page({
     enableprogressgesture: true,
     showmutebtn: true,
     objectfit: 'contain',
+
+    tab: 0
+
   },
   onLoad: function (options) {
+    
     // console.log(mv_url);
     const mvid = options.id; // onLoad()后获取到歌曲视频之类的id
 
@@ -47,5 +51,23 @@ Page({
       }
     })
   },
+  tab_slide: function (e) {//滑动切换tab 
+    var that = this;
+    that.setData({ tab: e.detail.current });
+  },
+  tab_click: function (e) {//点击tab切换
+    var that = this;
+    if (that.data.tab === e.target.dataset.current) {
+      return false;
+    } else {
+      that.setData({
+        tab: e.target.dataset.current
+      })
+    }
+  },
+
+
+
+
 })
 
